7b8fc02cc30ae998f1f1b9e07c47225c9ccb13e3
hdlilin <3221664152@qq.com>
2024-08-30 21:01:15 +0800

    feat: 添加依赖库，src别名配置

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,15 @@
   <div>
     <h1>李雪琳天天开心</h1>
     <h1>雪琳平平安安</h1>
+    <el-button type="primary" round :icon="Plus">点击雪琳好运运+1</el-button>
+    <el-button type="primary" round>点击雪琳好运运+1</el-button>
+
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
 
 <script setup lang="ts">
-let n = 5
-for (let i = 0; i < n; i++) {
-  console.log(i)
-}
-
+  import { Plus } from '@element-plus/icons-vue';
 </script>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// 引入element-plus插件与样式
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+const app = createApp(App)
+// @ts-ignore
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+app.use(ElementPlus, {
+    locale: zhCn
+})
+app.use(ElementPlus)
+app.mount('#app')
