501a9c51063db1f61d4821bf308c880d794da77b
hdlilin <3221664152@qq.com>
2024-08-30 19:51:09 +0800

    format

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>李雪琳天天开心</h1>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<script setup lang="ts">
+let n = 5
+for (let i = 0; i < n; i++) {
+  console.log(i)
+}
+
+</script>
+
+<style scoped></style>
